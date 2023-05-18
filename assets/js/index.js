418dfb11537d650226871f1d2c16b32532858087
Gabriela Pereira da Costa <gabriela-pc@hotmail.com>
2023-05-18 11:07:53 -0300

    Mario Game | Study Of JS

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout (() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+document.addEventListener('keydown', jump)
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './assets/img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        
+        load();
+
+        clearInterval(loop);
+    }
+},10);
+
+function load () {
+    setTimeout (() => {
+        location.reload();
+    }, 3000)
+    
+}
+
